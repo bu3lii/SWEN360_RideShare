fd60675602dbe0168a80b6e4be2fce2bb5643a64
mohamedalzayani13-arch <mohamed.alzayani13@gmail.com>
2025-10-27 18:41:11 +0300

    chore(app): add ride card widget placeholder, done by Mohamed AlZayani

---
+++ app/lib/widgets/ride_card.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class RideCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text('Ride Details'),
+        subtitle: Text('Placeholder for ride info'),
+      ),
+    );
+  }
+}
